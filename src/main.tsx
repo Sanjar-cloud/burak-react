bafaa28c405782123459f3500f67f9e0813dab7f
sanjar <sanjarbekkarimov229@gmail.com>
2026-06-26 06:21:56 +0900

    feat: MUI standard integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
 import './css/index.css'
 import App from './App.tsx'
+import theme from './thema'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
+
