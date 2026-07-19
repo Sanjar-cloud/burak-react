113174ade13111368fcba44b38e96e198e777234
sanjar <sanjarbekkarimov229@gmail.com>
2026-07-20 04:38:42 +0900

    feat: develop slice and selector usage

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,7 +6,30 @@ import PopularDishes from "./PopularDishes";
 import Statistics from "./statistics";
 import "../../../css/home.css";
 
+import { useDispatch, useSelector } from "react-redux";
+import type { Dispatch } from "@reduxjs/toolkit";
+import { createSelector } from "reselect";
+import { setPopularDishes } from "./slice";
+import { retrievePopularDishes } from "./selector";
+import type { Product } from "../../../lib/types/product";
+import { useEffect } from "react";
+
+/** redux slice & selector */
+const actionDispatch = (dispatch: Dispatch) => ({
+  setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+});
+const popularDishesRetriever = createSelector(
+  retrievePopularDishes,
+  (popularDishes) => ({ popularDishes })
+);
+
+
 export default function HomePage() {
+ const { setPopularDishes } = actionDispatch(useDispatch());
+ const { popularDishes } = useSelector(popularDishesRetriever);
+
+  useEffect(() => {}, []);
+  
   return <div className={ "homepage "}>
     <Statistics />
     <PopularDishes />
--- src/app/screens/homePage/selector.ts
+++ src/app/screens/homePage/selector.ts
@@ -17,3 +17,5 @@ export const retrieveTopUsers = createSelector(
   selectHomePage,
   (HomePage) => HomePage.topUsers
 );
+
+
