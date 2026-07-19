1d6c242dfa233ff9006e5c4f30ff9ae5c4ef3797
sanjar <sanjarbekkarimov229@gmail.com>
2026-07-20 02:49:02 +0900

    feat: develop AppRootState and HomePage Type integration

---
+++ src/lib/types/screen.ts
@@ -0,0 +1,18 @@
+import type { Member } from "./member";
+import type { Product } from "./product";
+
+/** React app Sate **/
+export interface AppRootState {
+  homePage: HomePageState;
+//   productsPage: ProductsPageState;
+}
+
+/** homePage  **/
+export interface HomePageState {
+  popularDishes: Product[];
+  newDished: Product[];
+  topUsers: Member[];
+}
+
+/** Products Page  **/
+/** Orders Page  **/
