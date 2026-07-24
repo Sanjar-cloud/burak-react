56ca7deddf88a125807f26d6401535c6fa46d7ba
sanjar <sanjarbekkarimov229@gmail.com>
2026-07-24 19:07:12 +0900

    feat: retreive topUsers and modify ActiveUsers component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -6,22 +6,26 @@ import PopularDishes from "./PopularDishes";
 import Statistics from "./statistics";
 import { useDispatch } from "react-redux";
 import type { Dispatch } from "@reduxjs/toolkit";
-import { setNewDishes, setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import type { Product } from "../../../lib/types/product";
 import { useEffect } from "react";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import type { Member } from "../../../lib/types/member";
 import "../../../css/home.css";
+import MemberService from "../../services/MemberService";
 
 /** redux slice & selector */
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
   setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data))
+
 });
 
 
 export default function HomePage() {
- const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
+ const { setPopularDishes, setNewDishes,setTopUsers } = actionDispatch(useDispatch());
 
 
 
@@ -51,6 +55,12 @@ export default function HomePage() {
       setNewDishes(data);
     })
     .catch((err) => console.log(err));
+     
+    const member = new  MemberService();
+    member.getTopUsers()
+    .then((data) => setTopUsers(data))
+    .catch((err) => console.log(err));
+
 }, []);
 
 
