1bc42917d71d08d9e8439bbd405da9d23625a365
sanjar <sanjarbekkarimov229@gmail.com>
2026-08-05 04:38:58 +0900

    feat: develop UpdateMember bussines logic

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -14,6 +14,7 @@ import type { Order, OrderInquiry } from "../../../lib/types/order";
 import { OrderStatus } from "../../../lib/enums/order.enum";
 import OrderService from "../../services/OrderService";
 import { useGlobals } from "../../hooks/useGlobals";
+import { useHistory } from "react-router-dom";
 import "../../../css/order.css";
 
 /** redux slice & selector */
@@ -26,7 +27,8 @@ const actionDispatch = (dispatch: Dispatch) => ({
 
 export default function OrdersPage() {
   const { setPausedOrders, setProcessOrders,setFinishedOrders } = actionDispatch(useDispatch());
-  const { orderBuilder} = useGlobals();
+  const { orderBuilder, authMember} = useGlobals();
+  const history = useHistory();
   const [value, setValue] = useState("1");
   const [orderInquiry, setOrderInquiry] = useState<OrderInquiry>({
   page: 1,
@@ -59,7 +61,7 @@ const handleChange = (_e: SyntheticEvent, newValue: string) => {
   setValue(newValue);
 };
 
-
+  if(!authMember) history.push("/");
   return (
     <div className={"order-page"}>
       <Container className="order-container">
